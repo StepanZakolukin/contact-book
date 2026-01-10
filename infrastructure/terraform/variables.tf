087521c37d09b1032ca9f772abde16514efb2993
stepan <stepan.zakolyukin@yandex.ru>
2026-01-10 14:08:30 +0500

    Скидал базовую структуру папок и файлов для настройки инфраструктуры

---
+++ infrastructure/terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "cloud_id" {
+  type        = string
+  description = "ID облака"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "ID директории в облаке"
+}
+
+variable "default_availability_zone" {
+  type        = string
+  description = "Зона доступности по умолчанию"
+}
