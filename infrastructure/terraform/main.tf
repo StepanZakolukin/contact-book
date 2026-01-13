88d12a12957ab8f3c9dd2d04ce08f0b606d04913
shestopalov <shestopalov@skbkontur.ru>
2026-01-13 17:23:25 +0500

    Добавили рабочую бд

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -91,6 +91,7 @@ resource "yandex_compute_instance" "database" {
   resources {
     cores  = 2
     memory = 2
+    core_fraction = 100
   }
 
   boot_disk {
@@ -108,6 +109,15 @@ resource "yandex_compute_instance" "database" {
 
   metadata = {
     ssh-keys = "yc-user:${var.ssh_key}"
+    user-data = <<-EOF
+      #cloud-config
+      runcmd:
+        - [ systemctl, start, docker ]
+        - [ systemctl, enable, docker ]
+        - [ mkdir, -p, /var/lib/postgresql/data ]
+        - [ chmod, 777, /var/lib/postgresql/data ]
+        - [ docker, run, -d, --name, postgres, --restart, always, -e, POSTGRES_PASSWORD=${var.postgres_pwd}, -p, 5432:5432, -v, /var/lib/postgresql/data:/var/lib/postgresql/data, postgres:16-alpine ]
+    EOF
   }
 
   lifecycle {
